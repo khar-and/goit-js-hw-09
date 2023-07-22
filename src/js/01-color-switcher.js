91f5f316d9589c9c8e442cb07daa8577583382f5
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-22 15:47:16 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let timerid = null;
+btnStop.setAttribute('disabled', true);
+
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+
+function onStartClick(evt) {
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+    
+    colorid = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStopClick(evt) {
+    clearInterval(colorid);
+    btnStop.setAttribute('disabled', true);
+    btnStart.removeAttribute('disabled');
+}
+
 
