476a3e2e7ad48e600d44819668260cc959e9cada
khar-and <126355265+khar-and@users.noreply.github.com>
2023-07-28 08:40:21 +0000

    Deploying to gh-pages from @ khar-and/goit-js-hw-09@eeb9e62c27c697932b7f520ba3a41d226a60b6ab 🚀

---
+++ 01-color-switcher.c0f99a8c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled",!0),t.addEventListener("click",(function(d){t.setAttribute("disabled",!0),e.removeAttribute("disabled");setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(r){clearInterval(colorid),e.setAttribute("disabled",!0),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.c0f99a8c.js.map
